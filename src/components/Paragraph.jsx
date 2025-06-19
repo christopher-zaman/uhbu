cc92f7a9a3a22ed72792a6bb7db5e10e39f2d496
Christopher Zaman <zamanchristopher@gmail.com>
2025-06-18 23:48:40 -0400

    updates

--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -1,7 +1,9 @@
 
-const Paragraph = ({ children, className = "" }) => {
+const Paragraph = ({ children, className = "", ...rest }) => {
   return (
-    <p className={`container text-center leading-relaxed text-gray-700 mb-4 ${className}`}>
+    <p className={`container text-center leading-relaxed text-gray-700 mb-4 ${className}`}
+    {...rest}
+    >
       {children}
     </p>
   );
